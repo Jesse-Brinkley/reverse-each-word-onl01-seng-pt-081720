7d1440f43f00f4c40fa2b7a0848b52ae30aa0996
Jesse Brinkley <jbrink28@gmail.com>
2020-08-27 16:36:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,8 @@
-def reverse_each_word("Hello there, how are you?")
-  reverse_each_word.reverse!
+def reverse_each_word(string)
+  original_array = string.split(" ")
+  return_array = []
+  original_array.each do|strong|
+    return_array << string.reverses
+  end
+  return_array.join(" ")
 end
